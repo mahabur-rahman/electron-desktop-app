af7e56e7e360efae9ed5a1f8a2e7f71dfe6cfcea
mahabur-rahman <annur4395@gmail.com>
2026-02-04 12:13:13 +0600

    Fix dev workflow and Squirrel startup

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,4 +1,5 @@
 import type { Configuration } from 'webpack';
+import path from 'node:path';
 
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
@@ -8,12 +9,19 @@ rules.push({
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
 });
 
-export const rendererConfig: Configuration = {
+export const rendererConfig: Configuration & { devServer?: unknown } = {
   module: {
     rules,
   },
   plugins,
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+  },
+  // Ensures changes to `src/index.html` trigger a full reload in dev.
+  // (Webpack's HMR mostly targets JS/CSS; HTML template changes may not be watched by default.)
+  devServer: {
+    hot: true,
+    liveReload: true,
+    watchFiles: [path.resolve(__dirname, 'src/index.html')],
   },
 };
